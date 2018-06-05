fe15690b0e01ed3a5755815de40e5dc8aca01195
Mr. Samuel <samuelsensei@gmail.com>
2018-06-04 18:42:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,106 +1,82 @@
-function betterWork(){
-  var theDate = new Date();
-  var theDateString = (theDate.getMonth() + 1) + "-" + theDate.getDate() + "-" + theDate.getFullYear();
-  var Dates = [
-    ["11-23-2017", "11-24-2017", "Happy Thanksgiving", 0, 0],
-    ["12-31-2018", "1-1-2019", "Happy New Year", 18, 0]
+function isItOpen (){
+  //Variables
+  var weekTimes = [
+    {closesAt: 1.5, opensAt: 10.5}, //Sunday
+    {closesAt: 1, opensAt: 10.5},
+    {closesAt: 1, opensAt: 10.5},
+    {closesAt: 1, opensAt: 10.5},
+    {closesAt: 1, opensAt: 10.5},
+    {closesAt: 1, opensAt: 10.5},
+    {closesAt: 1.5, opensAt: 10.5}, //Saturday
   ];
-  
-  for (i=0; i<Dates.length; i++){
-    if (Dates[i][0] == theDateString){ //if day of
-      if (Dates[i][3] > 0){ //if special timing
-        if ((theDate.getHours() == 10 && theDate.getMinutes() >= 30) || (theDate.getHours() >= 11 && theDate.getHours() < Dates[i][3]) || (theDate.getHours() == Dates[i][3] && theDate.getMinutes() < Dates[i][4])){ //if during special hours
-          document.getElementById("fillme").innerHTML = "<font color='green'>Open</font>";
-          //countdown from appointed time
-          var theSeconds = (theDate.getHours() * 60 * 60) + (theDate.getMinutes() * 60) + (theDate.getSeconds());
-          var theSeconds = (Dates[i][3] * 60 * 60) + (Dates[i][4] * 60) - theSeconds;
-          countMeDown(theSeconds);
-        }
-        else if (theDate.getHours() >= Dates[i][3] && theDate.getMinutes() >= Dates[i][4]){ //if after special hours
-          document.getElementById("fillme").innerHTML = "<font color='red'>Closed</font><br />";
-          document.getElementById("fillme").innerHTML += Dates[i][2];
-          return;
-        }
-      }
-      else if ((theDate.getHours() == 10 && theDate.getMinutes() >= 30) || (theDate.getHours() >= 11)){
-        document.getElementById("fillme").innerHTML = "<font color='red'>Closed</font><br />";
-        document.getElementById("fillme").innerHTML += Dates[i][2];
-        return;
-      }
+  var theDays = [
+    {fromDate:"June 4, 2018 7:00:00", toDate: "June 5, 2018 10:30:00", message: "Happy New Year!"},
+    {fromDate:"June 12, 2018 10:30:00", toDate: "June 14, 2018 10:30:00", message: "Happy New Year!"}
+  ];
+  var closedText = "<font color='red'>Closed</font>";
+  var openText = "<font color='green'>Open</font>";
+
+  //Constants
+  var theDate = new Date();
+  function theTime(){
+    return (theDate.getHours() + (theDate.getMinutes()/60) + (theDate.getSeconds()/(60*60)));
+  };
+  function fetchTime(nextDay){
+    var z = new Date();
+    if(nextDay == 1){
+      z.setTime(theDate.getTime() + (24*60*60*1000));
     }
-    else if (Dates[i][1] == theDateString){ //if day after
-      if (theDate.getHours() < 2){
-        document.getElementById("fillme").innerHTML = "<font color='red'>Closed</font><br />";
-        document.getElementById("fillme").innerHTML += Dates[i][2];
-        return;
-      }
-    }
-  }  
-  if ((theDate.getHours() == 1 && theDate.getMinutes() >= 30) || (theDate.getHours() == 10 && theDate.getMinutes() < 30) || (theDate.getHours() >=2 && theDate.getHours() < 10)){
-    document.getElementById("fillme").innerHTML = "<font color='red'>Closed</font>";
+    z.setHours(weekTimes[z.getDay()].closesAt/1);
+    z.setMinutes((weekTimes[z.getDay()].closesAt-Math.floor(weekTimes[z.getDay()].closesAt))*60);
+    z.setSeconds(0);
+    return z;
   }
-  else if (theDate.getHours() == 1 && theDate.getMinutes() < 30){
-    if (theDate.getDay() == 0 || theDate.getDay() == 6){
-      document.getElementById("fillme").innerHTML = "<font color='green'>Open</font>";
-      //countdown from 30 minutes
-      var theSeconds = (theDate.getHours() * 60 * 60) + (theDate.getMinutes() * 60) + (theDate.getSeconds());
-      var theSeconds = 3600 - theSeconds + (60*30);
-      countMeDown(theSeconds);
+  function getDateString(){
+    return ((theDate.getMonth() + 1) + "-" + theDate.getDate() + "-" + theDate.getFullYear());
+  }
+
+  //Logic
+  for (i=0; i<theDays.length; i++){
+    var a = new Date(theDays[i].fromDate);
+    var b = new Date(theDays[i].toDate);
+    if(theDate.getMonth() == a.getMonth() && theDate.getDate() == a.getDate() && theDate.getFullYear() == a.getFullYear() && theDate.getTime() < a.getTime()){
+      document.getElementById("fillme").innerHTML = openText;
+      countMeDown(a);
+      return;
+    }
+    else if (theDate.getTime() > a && theDate.getTime() < b){
+      document.getElementById("fillme").innerHTML = closedText + "<br />" + theDays[i].message;
+      return;
+    }
+  }
+  if (theTime() >= weekTimes[theDate.getDay()].closesAt && theTime() < weekTimes[theDate.getDay()].opensAt){
+    document.getElementById("fillme").innerHTML = closedText;
+  }
+  else {
+    document.getElementById("fillme").innerHTML = openText;
+    if (theTime() > weekTimes[theDate.getDay()].opensAt){
+      countMeDown(fetchTime(1));
     }
     else {
-      document.getElementById("fillme").innerHTML = "<font color='red'>Closed</font>";
+      countMeDown(fetchTime(0));
     }
   }
-  else if (theDate.getHours() < 1) {    
-    if (theDate.getDay() == 0 || theDate.getDay() == 6){
-      document.getElementById("fillme").innerHTML = "<font color='green'>Open</font>";
-      //countdown+30
-      var theSeconds = (theDate.getHours() * 60 * 60) + (theDate.getMinutes() * 60) + (theDate.getSeconds());
-      var theSeconds = 3600 - theSeconds + (60*30);
-      countMeDown(theSeconds);
-    }
-    else {
-      document.getElementById("fillme").innerHTML = "<font color='green'>Open</font>";
-      //countdown regular
-      var theSeconds = (theDate.getHours() * 60 * 60) + (theDate.getMinutes() * 60) + (theDate.getSeconds());
-      var theSeconds = 3600 - theSeconds;
-      countMeDown(theSeconds);
-    }
-  }
-  else if ((theDate.getHours() == 10 && theDate.getMinutes() >= 30) || (theDate.getHours() >= 11)) {    
-    if (theDate.getDay() == 5 || theDate.getDay() == 6){
-      document.getElementById("fillme").innerHTML = "<font color='green'>Open</font>";
-      //countdown+1:30
-      //86400 seconds in 24 hours
-      var theSeconds = (theDate.getHours() * 60 * 60) + (theDate.getMinutes() * 60) + (theDate.getSeconds());
-      var theSeconds = 86400 - theSeconds + (90*60);
-      countMeDown(theSeconds);
-    }
-    else {
-      document.getElementById("fillme").innerHTML = "<font color='green'>Open</font>";
-      //countdown+1:00
-      var theSeconds = (theDate.getHours() * 60 * 60) + (theDate.getMinutes() * 60) + (theDate.getSeconds());
-      var theSeconds = 86400 - theSeconds + (60*60);
-      countMeDown(theSeconds);
-    }
-  }
-  
-  function countMeDown(timeLeft){  
-    var endDate = theDate.getTime() + (timeLeft*1000);
+}
+
+function countMeDown(countDownTime){  
     var theInterval = setInterval (function(){
       var currentDate = new Date();
-      var timer = (endDate - currentDate.getTime())/1000;
+      var timer = (countDownTime - currentDate.getTime())/1000;
       var hours = Math.floor(timer/(60*60));
       var minutes = Math.floor(timer%(60*60)/60);
       var seconds = Math.floor(timer%(60*60)%60);
       document.getElementById("timeleft").innerHTML = hours + "h " + minutes + "m " + seconds + "s";
       if (timer <= 0.4){
-        document.getElementById("fillme").innerHTML = "<font color='red'>Closed</font>";
+        document.getElementById("fillme").innerHTML = closedText;
         document.getElementById("timeleft").innerHTML = "0h 0m 0s";
         clearInterval(theInterval);
       }
     }, 1000);
   }
-}  
 
-betterWork();
+isItOpen();
